e7593b2093194271da2b0675bbd33a8f3a5ea90b
orozbai98 <orozbaimadanbekov@gmail.com>
2019-11-15 17:51:51 +0600

    a

---
+++ task2.py
@@ -0,0 +1,30 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the superDigit function below.
+def superDigit(n, k):
+    m = n * k
+
+    while True:
+        m = sum(list(map(int, m)))
+        m = str(m)
+        if len(m) == 1:
+            return m
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = nk[0]
+
+    k = int(nk[1])
+
+    result = superDigit(n, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
