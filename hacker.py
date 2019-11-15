a60b3a7aea247db0d2d7331c8865a92c2ffab1e3
Kbektur <56334266+kayevbb@users.noreply.github.com>
2019-11-15 22:59:55 +0600

    Create hacker.py

---
+++ hacker.py
@@ -0,0 +1,14 @@
+# part5
+
+import re
+
+q = int(input().strip())
+regex = r"h.*a.*c.*k.*e.*r.*r.*a.*n.*k"
+compiled_regex = re.compile(regex)
+for a0 in range(q):
+    s = input().strip()
+    result = compiled_regex.search(s)
+    if result == None:
+        print("NO")
+    else:
+        print("YES")
